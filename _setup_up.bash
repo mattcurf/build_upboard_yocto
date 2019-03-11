b5a00028a956cf7d447e91d4a19fa70c322a6dd7
Matt Curfman <matt@tecnovelty.com>
2019-03-11 04:45:31 +0000

    Fixed kernel version:

--- _setup_up.bash
+++ _setup_up.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#export LC_ALL=en_US.UTF-8
-#export LANG=en_US.UTF-8
-#export LANGUAGE=en_US.UTF-8
-
 TEMPLATECONF=meta-up-board/conf source src/oe-init-build-env build_up
 bitbake-layers add-layer ../src/meta-openembedded/meta-filesystems
 bitbake-layers add-layer ../src/meta-openembedded/meta-python
@@ -18,11 +14,8 @@ SOURCE_MIRROR_URL = "file://$YOCTO_DOWNLOAD_CACHE"
 EXTRA_IMAGE_FEATURES = "tools-sdk tools-debug tools-profile"
 CORE_IMAGE_EXTRA_INSTALL += "rt-tests"
 DISTRO_FEATURES_append = " systemd virtualization kvm kubernetes"
-#DISTRO_FEATURES_remove = "x11 wayland"
 VIRTUAL-RUNTIME_init_manager = "systemd"
 IMAGE_INSTALL_append = "  docker docker-contrib connman connman-client"
 PREFERRED_PROVIDER_virtual/kernel = "linux-intel"
-# PREFERRED_PROVIDER_virtual/kernel ?= "linux-yocto"
-PREFERRED_VERSION_linux-intel = "4.19%"
-"
+PREFERRED_VERSION_linux-intel = "4.14k%"
 EOF
